2de895cd55187471418004af16f9c35366551525
Viet-UET <vietmtruong@gmail.com>
2026-03-09 21:59:28 +0700

    Init Spring boot

---
+++ src/main/java/com/example/financemanagementapi/FinanceManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.financemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/financemanagementapi/FinanceManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
